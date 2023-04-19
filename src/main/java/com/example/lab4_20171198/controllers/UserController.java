cb18b0f004c5bb09bc070b5065efa85d24ed4eb2
lucho2000 <luis.ramost@pucp.edu.pe>
2023-04-18 21:58:23 -0500

    cambios con fe

--- src/main/java/com/example/lab4_20171198/controllers/UserController.java
+++ src/main/java/com/example/lab4_20171198/controllers/UserController.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
 
 @Controller
 public class UserController {
@@ -22,18 +25,36 @@ public class UserController {
     /*public String nuevo() {
         return "hospital/nuevohospital";
     }*/
-    @GetMapping("/")
+    @GetMapping("/homepage")
     public String homePage(Model model){
 
-        return "homePage";
+        return "/homePage";
     }
 
 
-    @GetMapping("/inicio")
+    @GetMapping("/")
     public String inicio(Model model){
 
         return "inicio";
     }
+
+    @GetMapping("/")
+    public String compararUsers(Model model, @RequestParam("name") String name, @RequestParam("contrasena") String contrasena ){
+
+        model.addAttribute("users", userRepository.findAll());
+
+        Optional<User> optUser = userRepository.compararUsers(name, contrasena);
+
+        if (optUser.isPresent()) {
+            User user = optUser.get();
+            model.addAttribute("user", user);
+            return "/homePage";
+        } else {
+            return "redirect:/inicio";
+        }
+
+    }
+
 
     @PostMapping("/guardar")
     public String guardar(User user) {
--- src/main/java/com/example/lab4_20171198/repository/UserRepository.java
+++ src/main/java/com/example/lab4_20171198/repository/UserRepository.java
@@ -2,6 +2,12 @@ package com.example.lab4_20171198.repository;
 
 import com.example.lab4_20171198.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User,Integer> {
+
+    @Query(value = "select * from users where email=?1 and password=?2")
+    List<User> compararUsers(String name, String contrasena);
 }
