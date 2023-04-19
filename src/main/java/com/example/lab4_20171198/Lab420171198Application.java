e10c35633b6f7e4d2ac7e662b0e360f54614b555
lucho2000 <luis.ramost@pucp.edu.pe>
2023-04-18 20:30:38 -0500

    first commit

---
+++ src/main/java/com/example/lab4_20171198/Lab420171198Application.java
@@ -0,0 +1,13 @@
+package com.example.lab4_20171198;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab420171198Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab420171198Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lab4_20171198/Lab420171198ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab4_20171198;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab420171198ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
