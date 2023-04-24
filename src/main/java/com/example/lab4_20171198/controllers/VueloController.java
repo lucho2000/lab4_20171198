66e893124d307bc7dd507cf1204753e38c8a0491
unknown <luis.ramost@pucp.edu.pe>
2023-04-23 21:40:34 -0500

    crea un registro con ele stado

--- src/main/java/com/example/lab4_20171198/controllers/VueloController.java
+++ src/main/java/com/example/lab4_20171198/controllers/VueloController.java
@@ -1,24 +1,34 @@
 package com.example.lab4_20171198.controllers;
 
+import com.example.lab4_20171198.entity.Reserva;
+import com.example.lab4_20171198.entity.User;
+import com.example.lab4_20171198.entity.Vuelo;
 import com.example.lab4_20171198.repository.ReservaRepository;
+import com.example.lab4_20171198.repository.UserRepository;
 import com.example.lab4_20171198.repository.VueloRepository;
+import jdk.jshell.execution.LocalExecutionControl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.time.LocalDateTime;
 
 
 @Controller
 public class VueloController {
 
     final VueloRepository vueloRepository;
+    final UserRepository userRepository;
 
     final ReservaRepository reservaRepository;
 
-    public VueloController(VueloRepository vueloRepository, ReservaRepository reservaRepository) {
+    public VueloController(VueloRepository vueloRepository, ReservaRepository reservaRepository, UserRepository userRepository) {
         this.vueloRepository = vueloRepository;
         this.reservaRepository = reservaRepository;
+        this.userRepository = userRepository;
     }
 
     @GetMapping("/home")
@@ -28,10 +38,19 @@ public class VueloController {
         return "homePage";
     }
 
-    @PostMapping("/guardar")
-    public String guardar(Integer id, String estado) {
-        reservaRepository.cambioEstadoPago(estado, id); //llamamos al metodo para actualizar el registro
-
+    @GetMapping("/guardar")
+    public String guardar(Integer id, Integer user, RedirectAttributes attr) {
+        Vuelo vuelo = vueloRepository.findById(id).get();
+        User user1 = userRepository.findById(user).get();
+        Reserva reserva = new Reserva();
+        reserva.setVuelo(vuelo);
+        reserva.setUser(user1);
+        reserva.setFechaReserva(LocalDateTime.now().toString());
+        reserva.setPrecioTotal(vuelo.getPrecio());
+        reserva.setEstadoPago("procesado");
+        reservaRepository.save(reserva);
+        attr.addFlashAttribute("modal", "S");
+        attr.addFlashAttribute("usr", user1);
         return "redirect:/home";
     }
 }
--- src/main/java/com/example/lab4_20171198/repository/ReservaRepository.java
+++ src/main/java/com/example/lab4_20171198/repository/ReservaRepository.java
@@ -9,9 +9,4 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface ReservaRepository extends JpaRepository<Reserva,Integer> {
 
-
-    @Transactional
-    @Modifying
-    @Query(nativeQuery = true, value = "update reserva set estado_pago=?1 where idreserva=?2")
-    void cambioEstadoPago(String estado, Integer id);
 }
